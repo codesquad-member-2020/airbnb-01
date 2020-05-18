270404e9d02be29f3eef0b2987a74f420e584a7d
Byung Hyun Choi <choi8608@gmail.com>
2020-05-18 22:02:46 +0900

    [BE] chore: Init project
    - Init project

---
+++ BE/src/main/java/com/codesquad/airbnb01/Airbnb01Application.java
@@ -0,0 +1,12 @@
+package com.codesquad.airbnb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airbnb01Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Airbnb01Application.class, args);
+  }
+}
