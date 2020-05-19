67e02e72ebdb1383d628b04941d242400f6456b1
kyungrae <rlarudfo93@gmail.com>
2020-05-19 19:41:02 +0900

    [BE] feat: 모듈 단위로 테스트 코드를 위한 설정
    - Accommodation, Booking, User를 모듈 단위로 설정하고 테스트의 경우 필요한 Context만 로드하기 위해 설정 파일을 customizing 했다
    
    #6

---
+++ BE/airbnb/src/test/java/com/codesquad/airbnb/booking/BookingTestConfiguration.java
@@ -0,0 +1,10 @@
+package com.codesquad.airbnb.booking;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+@SpringBootConfiguration
+class BookingTestConfiguration extends BookingConfiguration {
+
+}
