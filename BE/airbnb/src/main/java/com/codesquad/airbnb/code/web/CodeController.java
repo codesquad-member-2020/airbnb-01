528946f05890b6a716db20ea4aa612c1992b6c40
Byung Hyun Choi <choi8608@gmail.com>
2020-05-20 21:23:49 +0900

    [BE] feat: 코드 값 확인을 위한 API 추가
    - 코드 값 확인을 위한 API 추가

---
+++ BE/airbnb/src/main/java/com/codesquad/airbnb/code/web/CodeController.java
@@ -0,0 +1,27 @@
+package com.codesquad.airbnb.code.web;
+
+import com.codesquad.airbnb.accmmodation.data.type.ImageType;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "dev")
+@RequiredArgsConstructor
+@RequestMapping("/dev-api/")
+@RestController
+public class CodeController {
+
+  @ApiOperation(value = "Code 값을 가져옵니다")
+  @GetMapping
+  public Enum[] accommodation(@RequestParam String type) {
+    if (type.equals("image")) {
+      return ImageType.values();
+    }
+
+    throw new RuntimeException("올바르지 않은 입력 값 입니다");
+  }
+}
