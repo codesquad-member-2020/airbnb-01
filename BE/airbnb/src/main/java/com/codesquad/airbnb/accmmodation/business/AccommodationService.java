ac024c132365ad2e6cccb52a33ee12861bda4882
Byung Hyun Choi <choi8608@gmail.com>
2020-05-20 21:23:20 +0900

    [BE] feat: detailAccommodation 추가
    - detailAccommodation 추가

--- BE/airbnb/src/main/java/com/codesquad/airbnb/accmmodation/business/AccommodationService.java
+++ BE/airbnb/src/main/java/com/codesquad/airbnb/accmmodation/business/AccommodationService.java
@@ -1,7 +1,12 @@
 package com.codesquad.airbnb.accmmodation.business;
 
+import com.codesquad.airbnb.accmmodation.data.Accommodation;
 import com.codesquad.airbnb.accmmodation.data.AccommodationRepository;
-import com.codesquad.airbnb.accmmodation.web.SimpleAccommodationDto;
+import com.codesquad.airbnb.accmmodation.data.type.ImageType;
+import com.codesquad.airbnb.accmmodation.web.AccommodationDto;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +16,27 @@ public class AccommodationService {
 
   private final AccommodationRepository accommodationRepository;
 
-  public SimpleAccommodationDto accommodation() {
-    return SimpleAccommodationDto.builder()
-        .accommodations(accommodationRepository.findAll())
+  public List<AccommodationDto> accommodation() {
+    return accommodationRepository.findAll().stream().map(
+        accommodation -> AccommodationDto.builder()
+            .id(accommodation.getId())
+            .name(accommodation.getName())
+            .images(accommodation.getImages().stream()
+                .filter(image -> image.getType().equals(ImageType.MAIN))
+                .collect(Collectors.toList()))
+            .price(accommodation.getPrice())
+            .build()).collect(Collectors.toList());
+  }
+
+  public AccommodationDto detailAccommodation(Long id) {
+    Accommodation accommodation = accommodationRepository.findById(id)
+        .orElseThrow(NoSuchElementException::new);
+
+    return AccommodationDto.builder()
+        .id(accommodation.getId())
+        .name(accommodation.getName())
+        .images(accommodation.getImages())
+        .coordinate(accommodation.getCoordinate())
         .build();
   }
 }
