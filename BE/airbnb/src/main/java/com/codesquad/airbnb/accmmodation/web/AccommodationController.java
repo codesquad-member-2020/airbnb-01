8b5eb01424708ef215939e9bcf01212f7a237b9d
Byung Hyun Choi <choi8608@gmail.com>
2020-05-20 17:05:01 +0900

    [BE] feat: Swagger 설정
    - Swagger 설정
    
    #10

--- BE/airbnb/src/main/java/com/codesquad/airbnb/accmmodation/web/AccommodationController.java
+++ BE/airbnb/src/main/java/com/codesquad/airbnb/accmmodation/web/AccommodationController.java
@@ -1,11 +1,14 @@
 package com.codesquad.airbnb.accmmodation.web;
 
 import com.codesquad.airbnb.accmmodation.business.AccommodationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "prod")
 @RequiredArgsConstructor
 @RequestMapping("/api/accommodation")
 @RestController
@@ -13,6 +16,7 @@ public class AccommodationController {
 
   private final AccommodationService accommodationService;
 
+  @ApiOperation(value = "", notes = "숙소 전체 리스트를 가져옵니다")
   @GetMapping
   public SimpleAccommodationDto accommodation() {
     return accommodationService.accommodation();
