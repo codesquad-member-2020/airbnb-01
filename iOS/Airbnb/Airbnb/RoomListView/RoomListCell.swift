bf7f654a13e8819386064a82dd38a3207010f9e5
1Consumption <hanseop95@gmail.com>
2020-05-20 17:15:18 +0900

    [iOS] refactor: cell을 xib로 변경
    - 추후 동일한 cell을 사용하는 부분이 있어서 재사용을 위해 xib로 변경
    - pageControl 연동함.
    
    #4

--- iOS/Airbnb/Airbnb/RoomListView/RoomListCell.swift
+++ iOS/Airbnb/Airbnb/RoomListView/RoomListCell.swift
@@ -8,8 +8,44 @@
 
 import UIKit
 
-class RoomListCell: UICollectionViewCell {
+@IBDesignable class RoomListCell: UICollectionViewCell {
     
+    private let xibName = String(describing: RoomListCell.self)
+    
+    @IBOutlet var view: UIView!
     @IBOutlet weak var likeButton: LikeButton!
-    @IBOutlet weak var ImageStackView: UIStackView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var imageScrollView: UIScrollView!
+    @IBOutlet weak var imageStackView: UIStackView!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setXib()
+        imageScrollView.delegate = self
+        pageControl.numberOfPages = imageStackView.subviews.count
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setXib()
+        imageScrollView.delegate = self
+        pageControl.numberOfPages = imageStackView.subviews.count
+    }
+    
+    private func setXib() {
+        let bundle = Bundle(for: RoomListCell.self)
+        bundle.loadNibNamed(xibName, owner: self, options: nil)
+        addSubview(view)
+        view.frame = self.bounds
+        view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
+
+extension RoomListCell: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let currentOffset: Int = Int(scrollView.contentOffset.x)
+        if currentOffset % Int(scrollView.frame.width) == 0 {
+            pageControl.currentPage = currentOffset / Int(scrollView.frame.width)
+        }
+    }
 }
