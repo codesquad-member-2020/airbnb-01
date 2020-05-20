bcf10ced409a1bfcd37a106ab051f8f7cc96ec9a
Byung Hyun Choi <choi8608@gmail.com>
2020-05-20 21:51:07 +0900

    [BE] refactor: DTO 구현 위치 변경
    - Accommodation 들의 Dto 를 관장하는 슈퍼 Dto 생성
    - Dto 안의 create 메소드를 통해 적절한 Dto 를 생성합니다

--- BE/airbnb/src/main/java/com/codesquad/airbnb/accmmodation/web/AccommodationController.java
+++ BE/airbnb/src/main/java/com/codesquad/airbnb/accmmodation/web/AccommodationController.java
@@ -1,6 +1,7 @@
 package com.codesquad.airbnb.accmmodation.web;
 
 import com.codesquad.airbnb.accmmodation.business.AccommodationService;
+import com.codesquad.airbnb.accmmodation.data.Accommodation;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.util.List;
@@ -17,16 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccommodationController {
 
   private final AccommodationService accommodationService;
+  private final AccommodationDto accommodationDto;
 
   @ApiOperation(value = "숙소 전체 리스트를 가져옵니다")
   @GetMapping
-  public List<AccommodationDto> accommodation() {
-    return accommodationService.accommodation();
+  public List<AccommodationDto> accommodations() {
+    List<Accommodation> accommodations = accommodationService.accommodation();
+    return accommodationDto.createAccmmodations(accommodations);
   }
 
   @ApiOperation(value = "득정 숙소의 상세 정보를 가져옵니다")
   @GetMapping("/{id}")
   public AccommodationDto detailAccommodation(@PathVariable Long id) {
-    return accommodationService.detailAccommodation(id);
+    Accommodation accommodation = accommodationService.detailAccommodation(id);
+    return accommodationDto.createAccmmodation(accommodation);
   }
 }
--- BE/airbnb/src/main/java/com/codesquad/airbnb/accmmodation/web/AccommodationDto.java
+++ BE/airbnb/src/main/java/com/codesquad/airbnb/accmmodation/web/AccommodationDto.java
@@ -1,11 +1,14 @@
 package com.codesquad.airbnb.accmmodation.web;
 
+import com.codesquad.airbnb.accmmodation.data.Accommodation;
 import com.codesquad.airbnb.accmmodation.data.Coordinate;
 import com.codesquad.airbnb.accmmodation.data.Image;
 import com.codesquad.airbnb.accmmodation.data.Price;
+import com.codesquad.airbnb.accmmodation.data.type.ImageType;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -19,4 +22,25 @@ public class AccommodationDto {
   private final Coordinate coordinate;
   private final List<Image> images;
   private final Price price;
+
+  public List<AccommodationDto> createAccmmodations(List<Accommodation> accommodations) {
+    return accommodations.stream().map(
+        accommodation -> AccommodationDto.builder()
+            .id(accommodation.getId())
+            .name(accommodation.getName())
+            .images(accommodation.getImages().stream()
+                .filter(image -> image.getType().equals(ImageType.MAIN))
+                .collect(Collectors.toList()))
+            .price(accommodation.getPrice())
+            .build()).collect(Collectors.toList());
+  }
+
+  public AccommodationDto createAccmmodation(Accommodation accommodation) {
+    return AccommodationDto.builder()
+        .id(accommodation.getId())
+        .name(accommodation.getName())
+        .images(accommodation.getImages())
+        .coordinate(accommodation.getCoordinate())
+        .build();
+  }
 }
