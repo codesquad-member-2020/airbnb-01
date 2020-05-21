bf4eb4bdb523904beb9bc4b0312db12be5c28718
1Consumption <hanseop95@gmail.com>
2020-05-21 21:54:27 +0900

    [iOS] feat: Room struct 생성
    - 서버로부터 방 목록을 받을 Model
    
    #18

---
+++ iOS/Airbnb/Airbnb/Model/RoomList.swift
@@ -0,0 +1,26 @@
+//
+//  RoomList.swift
+//  Airbnb
+//
+//  Created by 신한섭 on 2020/05/21.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+struct Room: Decodable {
+    var id: Int
+    var images: [Image]
+    var name: String
+    var price: Price
+    var type: String
+}
+
+struct Image: Decodable {
+    var type: String
+    var url: String
+}
+
+struct Price: Decodable {
+    var price: Int
+}
