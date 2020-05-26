e973db1e27c959b63baf8fe83e16a46f5572027b
Byung Hyun Choi <choi8608@gmail.com>
2020-05-26 21:13:53 +0900

    [BE] feat: login 기능 추가
    - AuthService
      - 로그인 관련 기능 구현
    - TokenUtil
      - jwt 토큰 관련 기능 구현
    
    #44

--- BE/airbnb/src/main/java/com/codesquad/airbnb/exception/CustomAdvice.java
+++ BE/airbnb/src/main/java/com/codesquad/airbnb/exception/CustomAdvice.java
@@ -1,12 +1,15 @@
 package com.codesquad.airbnb.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomAdvice {
 
   @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public Object handleException(Exception e) {
     return e;
   }
