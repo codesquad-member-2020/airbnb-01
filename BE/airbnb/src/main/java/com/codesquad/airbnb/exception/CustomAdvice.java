8889bf4b04a1b30325ab4b46cf7b267d63b027f9
Byung Hyun Choi <choi8608@gmail.com>
2020-05-27 17:45:15 +0900

    [BE] chore: validator 에러 추가
    - validator 에러 추가

--- BE/airbnb/src/main/java/com/codesquad/airbnb/exception/CustomAdvice.java
+++ BE/airbnb/src/main/java/com/codesquad/airbnb/exception/CustomAdvice.java
@@ -1,6 +1,7 @@
 package com.codesquad.airbnb.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,12 @@ public class CustomAdvice {
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public Object handleException(Exception e) {
-    return e;
+    return e.getMessage();
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    return e.getMessage();
   }
 }
