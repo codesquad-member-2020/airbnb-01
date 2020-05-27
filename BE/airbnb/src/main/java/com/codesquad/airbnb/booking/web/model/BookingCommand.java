4132b0b4408c3b259f9457a79d6b3357e2c249be
Byung Hyun Choi <choi8608@gmail.com>
2020-05-27 21:27:50 +0900

    [BE] refactor: client 의 테스트 용이성을 위해 example 값 삽입
    - client 의 테스트 용이성을 위해 example 값 삽입
    
    #48

--- BE/airbnb/src/main/java/com/codesquad/airbnb/booking/web/model/BookingCommand.java
+++ BE/airbnb/src/main/java/com/codesquad/airbnb/booking/web/model/BookingCommand.java
@@ -1,5 +1,6 @@
 package com.codesquad.airbnb.booking.web.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
@@ -14,15 +15,19 @@ import org.springframework.validation.annotation.Validated;
 @Getter
 public class BookingCommand {
 
+  @ApiModelProperty(value = "숙소 ID", required = true, example = "2")
   @NotNull
   private final Long accommodationId;
 
+  @ApiModelProperty(value = "체크 인 일자", required = true, example = "2020-06-17")
   @FutureOrPresent
   private final LocalDate checkIn;
 
+  @ApiModelProperty(value = "체크 아웃 일자", required = true, example = "2020-06-20")
   @Future
   private final LocalDate checkOut;
 
+  @ApiModelProperty(value = "투숙객 수", required = true, example = "2")
   @Range(min = 1, max = 9)
   private final Integer personCount;
 }
