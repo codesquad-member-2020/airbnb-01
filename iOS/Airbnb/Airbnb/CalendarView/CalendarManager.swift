4ad22101aff271b1fb6c13aab973438e93f75958
1Consumption <hanseop95@gmail.com>
2020-06-03 04:01:56 +0900

    [iOS] feat: 날짜 모델을 관리하는 객체 생성
    - 현재부터 1년사이의 날짜 정보를 생성하고 관리함
    - 1년 단위로 날짜 생성 가능(paging을 고려)
    
    #61

---
+++ iOS/Airbnb/Airbnb/CalendarView/CalendarManager.swift
@@ -0,0 +1,49 @@
+//
+//  CalendarManager.swift
+//  Airbnb
+//
+//  Created by 신한섭 on 2020/06/03.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+class CalendarManager {
+    private let daysArr = ["일", "월", "화", "수", "목", "금", "토"]
+    var months: [MonthInfo]
+    private var currentYear = Calendar.current.component(.year, from: Date())
+    private var currentMonth = Calendar.current.component(.month, from: Date())
+
+    init() {
+        months = [MonthInfo]()
+        yearInfo()
+    }
+
+    func yearInfo() {
+        for _ in 0...12 {
+            let startOfMonth = weekDay(year: currentYear, month: currentMonth)
+            let totalDay = Calendar.current.component(.day, from: "\(currentYear)-\(currentMonth)-01".convertDate().endOfMonth)
+
+            let monthInfo = MonthInfo(month: currentMonth, startOfMonth: startOfMonth, totalDays: totalDay, year: currentYear)
+            months.append(monthInfo)
+            
+            if currentMonth + 1 > 12 {
+                currentMonth = 1
+                currentYear += 1
+            } else {
+                currentMonth += 1
+            }
+        }
+    }
+    
+    func weekDay(year: Int, month: Int) -> Int {
+        return Calendar.current.component(.weekday, from: "\(year)-\(month)-01".convertDate())
+    }
+}
+
+struct MonthInfo {
+    let month: Int
+    let startOfMonth: Int
+    let totalDays: Int
+    let year: Int
+}
